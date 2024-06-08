25f4772200b096ba77ee8171069ef850e2ef7672
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-06-08 21:16:11 +0300

    [Initial commit]

---
+++ RecentTouchOnlyInput/Readme.md
@@ -0,0 +1,31 @@
+﻿# Recent Touch Only Input Package
+
+This package provides a touch input module where the next touch cancels the previous one.
+
+## Installation
+
+1. Add the following to your `manifest.json` in the `Packages` folder of your Unity project:
+    ```json
+    {
+      "dependencies": {
+        "com.yourname.recenttouchonlyinput": "https://github.com/yourusername/your-repo.git"
+      }
+    }
+    ```
+
+2. Open Unity and it will automatically download and import the package.
+
+## Usage
+
+1. Drag and drop the `RecentTouchOnlyInput.prefab` from the `Prefabs` folder onto the scene where you want the new touch logic.
+2. If you want the prefab to persist between scenes, do not destroy it between scenes.
+
+## API
+
+### Events
+
+- **Action<Touch> OnTouchCancel**: This event fires every time the touch gets cancelled and a new touch starts processing. It passes the touch that got cancelled.
+
+### Properties
+
+- **Touch MostRecentTouch**: This property returns the touch that is currently occurring.
---
+++ RecentTouchOnlyInput/Runtime/RecentTouchOnlyInput.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace OriGames.RecentTouchOnlyInput
+{
+	public class RecentTouchOnlyInput : BaseInput
+	{
+		/// <summary>
+		/// Fires every time the touch get cancelled and the recent one starts to process. Passes the touch that got cancelled
+		/// </summary>
+		public event Action<Touch> OnTouchCancel = delegate { }; 
+		
+		/// <summary>
+		/// Returns the touch that is occuring currently
+		/// </summary>
+		public Touch MostRecentTouch => GetTouchByFingerId(_mostRecentTouchFingerId);
+		
+		private int _mostRecentTouchFingerId = -1;
+
+		public void ProcessTouches()
+		{
+			for (int i = 0; i < touchCount; i++)
+			{
+				var touch = GetTouch(i);
+
+				if (touch.phase != TouchPhase.Began)
+				{
+					continue;
+				}
+
+				OnTouchCancel.Invoke(GetTouchByFingerId(_mostRecentTouchFingerId));
+                    
+				_mostRecentTouchFingerId = touch.fingerId;
+			}
+		}
+
+		private Touch GetTouchByFingerId(int fingerId)
+		{
+			foreach (var touch in Input.touches)
+			{
+				if (touch.fingerId == fingerId)
+				{
+					return touch;
+				}
+			}
+
+			return default;
+		}
+	}
+}
